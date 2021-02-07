a9082ed3e9a197e6c164901070901f1999c07038
田原隼併 <t.pe1shun@gmail.com>
2021-02-07 19:00:07 +0900

    2021/02/07

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
 // DOM生成が完了してから実行。
 document.addEventListener("DOMContentLoaded", function () {
   const hero = new HeroSlider(".swiper-container");
-//   hero.start({delay: 1000});
+  //   hero.start({delay: 1000});
   hero.start();
+
+  const cb = function (el, isIntersecting) {
+    if (isIntersecting) {
+      const ta = new TweenTextAnimation(el);
+      ta.animate();
+    }
+  };
+
+  const so = new ScrollObserver(".tween-animate-title", cb);
 });
